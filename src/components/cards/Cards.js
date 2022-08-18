dda1f4be3419f728b069e611859710296c7fb831
AshleyITR <ashley_brenner@itrsa.com.ar>
2022-08-18 09:23:33 -0300

    Update Cards.js

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -17,7 +17,33 @@ export default function Cards() {
         
 
         data.map(function (dato) {
-          setResponse(dato);
+          //setResponse(dato);
+
+          return (
+            <div className="Cards">
+              <main className="container">
+                <h1 className="text-warning" id="capsuleName">Capsules</h1>
+                <div id="imgs">
+                <div className="col-sm-6"><div className="card mb-3" >
+                    <h5 className="card-header">{dato.name}</h5>
+                    <img src={dato.flickr_images} className="card-img-top" alt="..."/>
+                   
+                    <div className="card-body">
+                      <p className="card-text">{dato.description}</p>
+                      <ul className="list-group list-group-flush">
+                      <li className="list-group-item"> <span className="badge bg-warning">Type: {dato.type}</span>
+                       <span className={estaActivo}>Is active: {dato.active}</span>
+                       <span className="badge bg-primary">Crew capacity: {dato.crew_capacity}</span></li>
+                      </ul>
+                    </div></div>
+                  </div>
+                </div>
+              </main>
+        
+        
+            </div>
+          );
+
         });
 
 
@@ -30,28 +56,5 @@ export default function Cards() {
 
 
 
-  return (
-    <div className="Cards">
-      <main className="container">
-        <h1 className="text-warning" id="capsuleName">Capsules</h1>
-        <div id="imgs">
-        <div className="col-sm-6"><div className="card mb-3" >
-            <h5 className="card-header">{response.dato.name}</h5>
-            <img src={response.dato.flickr_images} className="card-img-top" alt="..."/>
-           
-            <div className="card-body">
-              <p className="card-text">{response.dato.description}</p>
-              <ul className="list-group list-group-flush">
-              <li className="list-group-item"> <span className="badge bg-warning">Type: {response.dato.type}</span>
-               <span className={response.estaActivo}>Is active: {response.dato.active}</span>
-               <span className="badge bg-primary">Crew capacity: {response.dato.crew_capacity}</span></li>
-              </ul>
-            </div></div>
-          </div>
-        </div>
-      </main>
-
-
-    </div>
-  );
+  
 }
